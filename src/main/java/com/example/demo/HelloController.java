288e7da13732b7c513db09c337d3460af68732f4
ksqrt <ksqrt98@gmail.com>
2025-09-13 17:41:58 +0900

    spring init : Docker 설정 파일, Maven 관련 파일 및 Spring Boot 애플리케이션 구조 추가

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/api/hello")
+  public String hello() {
+    return "hello from spring: " + OffsetDateTime.now();
+  }
+}
